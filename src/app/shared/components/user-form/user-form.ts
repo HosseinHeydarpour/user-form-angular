cdf88a8d3ae8bdbdbcbf1d08ab3e652064c1dd26
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-17 20:29:14 +0330

    feat(WIP): add validation errors for the form

--- src/app/shared/components/user-form/user-form.ts
+++ src/app/shared/components/user-form/user-form.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DxTextBoxModule } from 'devextreme-angular';
+import { DxValidatorModule } from 'devextreme-angular';
+
+import { DxNumberBoxModule } from 'devextreme-angular';
+import { DxDateBoxModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-user-form',
-  imports: [DxTextBoxModule, ReactiveFormsModule],
+  imports: [
+    DxTextBoxModule,
+    ReactiveFormsModule,
+    DxValidatorModule,
+    DxNumberBoxModule,
+    DxDateBoxModule,
+  ],
   templateUrl: './user-form.html',
   styleUrl: './user-form.scss',
 })
@@ -13,24 +23,28 @@ export class UserForm {
 
   constructor() {
     this.userInfoForm = new FormGroup({
-      firstName: new FormControl<string>('', {
-        validators: Validators.required,
-      }),
-      lastName: new FormControl<string>('', {
-        validators: Validators.required,
-      }),
-      age: new FormControl<number>(0, {
-        validators: Validators.required,
-      }),
-      education: new FormControl<string>('', {
-        validators: Validators.required,
-      }),
-      nationalID: new FormControl<string>('', {
-        validators: Validators.required,
-      }),
-      birthDate: new FormControl<string>('', {
-        validators: Validators.required,
-      }),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      age: new FormControl(null, [Validators.required, Validators.min(0)]),
+      education: new FormControl('', Validators.required),
+      nationalID: new FormControl('', Validators.required),
+      birthDate: new FormControl('', Validators.required),
     });
   }
+
+  markAsTouched(controlName: string) {
+    this.userInfoForm.get(controlName)?.markAsTouched();
+  }
+
+  getError(controlName: string): string | null {
+    const control = this.userInfoForm.get(controlName);
+    if (!control || !control.touched || control.valid) return null;
+
+    if (control.errors?.['required']) return `${controlName} is required`;
+    if (control.errors?.['min'])
+      return `${controlName} must be greater than ${control.errors['min'].min}`;
+    return null;
+  }
+
+  onFormSubmission() {}
 }
