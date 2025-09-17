95a13715a5f9521c6261c3eec22d8f71c9b0f151
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-17 16:03:27 +0330

    feat: add data grid component to the app component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserGrid } from './features/user-grid/user-grid';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UserGrid],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
