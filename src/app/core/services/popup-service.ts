e91bc50244765cb991c3ac5a23ca9254a6d6e069
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-17 17:53:33 +0330

    feat: implement reactive

--- src/app/core/services/popup-service.ts
+++ src/app/core/services/popup-service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PopupService {
-  
+  private popupIsVisible = signal<boolean>(false);
+  popIsVisibleReadOnly = this.popupIsVisible.asReadonly;
+
+  openPop() {
+    this.popupIsVisible.set(true);
+  }
+  closePopup() {
+    this.popupIsVisible.set(false);
+  }
 }
--- src/app/shared/components/popup/popup.ts
+++ src/app/shared/components/popup/popup.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, effect, inject, signal } from '@angular/core';
+import { PopupService } from '../../../core/services/popup-service';
 
 @Component({
   selector: 'app-popup',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './popup.html',
-  styleUrl: './popup.scss'
+  styleUrl: './popup.scss',
 })
 export class Popup {
+  popupService = inject(PopupService);
 
+  get isVisible() {
+    return this.popupService.popIsVisibleReadOnly();
+  }
 }
