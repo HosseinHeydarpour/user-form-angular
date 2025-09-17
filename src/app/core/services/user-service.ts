d90553a2bf386827404b2561ba440dce4eace064
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-17 21:19:07 +0330

    fix: rename user service

---
+++ src/app/core/services/user-service.ts
@@ -0,0 +1,22 @@
+import { Injectable, signal } from '@angular/core';
+import { users } from '../dev-data/mock-users';
+import { IUser } from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  // Mock data coming from dev-data folder
+  private usersData = signal<IUser[]>(users);
+  singleUserData = signal<IUser | null>(null);
+
+  // Read OP
+  getUsers() {
+    return this.usersData();
+  }
+
+  setSingleUserData(data: IUser) {
+    this.singleUserData.set(data);
+    console.log(data);
+  }
+}
