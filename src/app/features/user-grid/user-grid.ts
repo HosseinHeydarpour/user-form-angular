f352977a4abea659ba405198853bf17ed919fe03
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-17 15:50:57 +0330

    feat: add user grid component

---
+++ src/app/features/user-grid/user-grid.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-grid',
+  imports: [],
+  templateUrl: './user-grid.html',
+  styleUrl: './user-grid.scss'
+})
+export class UserGrid {
+
+}
