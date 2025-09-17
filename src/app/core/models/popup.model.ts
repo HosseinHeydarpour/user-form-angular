c72bbf41d14892d5c978b93a1cccec2c97e5f20c
HosseinHeydarpour <hosseinheydarpour1@gmail.com>
2025-09-17 19:17:57 +0330

    feat: add button type and style to interface

--- src/app/core/models/popup.model.ts
+++ src/app/core/models/popup.model.ts
@@ -1,6 +1,8 @@
 export interface IPopupButton {
   title: string; // Text to display on the button
   action?: () => void; // Optional callback when button is clicked
+  type: 'default' | 'success' | 'danger' | 'normal' | 'back';
+  stylingMode: 'contained' | 'outlined' | 'text';
 }
 
 export interface IPopupConfig {
